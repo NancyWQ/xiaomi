b111e7c5a888fdde5ce45f7173659216ea3ea39b
NancyWQ <bb08280703@163.com>
2018-03-04 22:52:25 +0800

    提交文件

---
+++ src/components/home/header.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+ <div>
+      <i class="icon iconfont icon-xiaomi"></i>
+      <input type="text" placeholder="搜索商品名称">
+      <i class="icon iconfont icon-xiaolian"></i>
+      </div>
+      <ul>
+          <li v-for="tmp in title">{{tmp}}</li>
+      </ul>
+</div>
+</template>
+<script>
+export default {
+  name: 'HomeHeaderComponent',
+  data(){
+            return {title:["推荐","手机","智能","电视","电脑","全面屏","生活周边"]}
+        }
+}
+</script>
