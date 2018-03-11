fc1d408856b9c9cc130c63cc02c2108def1a6fd9
NancyWQ <bb08280703@163.com>
2018-03-11 22:30:50 +0800

    2018.3.11晚上

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -7,7 +7,7 @@
       </div>
       <nav class="menu-box scroll-box-x noscroll">
         <ul class="wrap-box clear">
-          <li class="item"  v-for="(value, index) in title">
+          <li class="item"  v-for="(value, index) in title" @click="chooseTitle(index)">
             <span>{{value}}</span>
           </li>
         </ul>
@@ -18,9 +18,18 @@
 <script>
 export default {
   name: 'HeaderComponent',
-  data(){
+  data:function(){
             return {title:["推荐","手机","智能","电视","电脑","全面屏","生活周边","盒子","艺术"]}
-        }
+        },
+  methods:{
+    // 选择是那一个题目的函数
+    chooseTitle:function(i){
+      // 2子组件给父组件传值--方法一
+      // 2.1.1使用$emit函数传给父组件
+      console.log(i);
+       this.$emit('title',i);
+    }
+  }
 }
 </script>
 <style lang="sass">
--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="app-init home-index">
       <!--1.3引入-->
-      <HeaderComponent></HeaderComponent>
+      <!--2.1.2在父组件中给子组件绑定事件-->
+      <HeaderComponent @title="thisTitle"></HeaderComponent>
       <p>这是中间</p>
       <FooterComponent></FooterComponent>
   </div>
@@ -13,10 +14,21 @@ import HeaderComponent from '../common/header'
 import FooterComponent from '../common/footer'
 export default {
 name:"HomeComponent",
+data:function(){
+  return {
+    title:0
+  } 
+},
 //1.2声明
 components: {
     HeaderComponent,FooterComponent
   },
+  methods:{
+    // 2.1.3定义函数，并进行处理
+    thisTitle:function(msg){
+      this.title=msg;
+    }
+  }
 }
 </script>
 <style lang="sass">
