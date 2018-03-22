7a50141babf9716457b2241ccc5dbc85eebefbe2
NancyWQ <bb08280703@163.com>
2018-03-22 22:28:35 +0800

    2018.3.22晚上

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -20,6 +20,34 @@
             <img  src="http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/2018011022290460400.png" alt="" style="width:100%;height:100%;">
           </a>
       </div>
+      <div class="shop-item clear">
+        <img src="http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/2018011114551289342.png" class="fl href" alt="" style="width:49.5%;margin-bottom: 0.05rem">
+        <img src="//cdn.cnbj0.fds.api.mi-img.com/b2c-mimall-media/e2ad0dbf777fda097e55cea9ce716e33.jpg?thumb=1&w=358&h=252" class="fr href" style="margin-bottom: 0.05rem;width:49.5%" alt="">
+        <img src="http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/2018011114550596671.png" class="fl href" alt="" style="width:49.5%">
+        <img src="http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/2018011114550596671.png" class="fr href" alt="" style="width:49.5%">
+      </div>
+      <div class="shop-item clear">
+        <img   src="http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/a589d24e74c4d96191dd46635054e804.jpg" alt="">
+      </div>
+      <div class="shop-item clear">
+        <img src="http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/2018011115034174111.png" alt="">
+      </div>
+      <div class="shop-item clear">
+        <img src="http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/2018011115033089933.png" alt="">
+      </div>
+      <div class="shop-item">
+        <div class="shop-box clear">
+
+          <div class="shop-box-item" v-for="target in shoplist">
+            <img :src="target.src" alt="">
+            <p class="title">{{ target.title }}</p>
+            <p class="con">{{ target.con }}</p>
+            <p class="money">
+              <span class="small">￥</span> {{ target.money }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
       <div class="scroll-box tab-box mi-tj footer-hack" v-if="title==1">
         <CarouselComponent></CarouselComponent>
@@ -59,7 +87,79 @@ export default {
 name:"HomeComponent",
 data:function(){
   return {
-    title:0
+    title:0,
+    shoplist : [
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          },
+          {
+            src: 'http://oz3tayfme.bkt.clouddn.com/show.liluo.cc/65eeeedca463345b0cfd36e042185af3.jpg',
+            title: '米家感应灯',
+            con: '举步之明，光明立现',
+            money: 49,
+            href: '/detail/1008'
+          }
+        ],
     } 
 },
 //1.2声明
