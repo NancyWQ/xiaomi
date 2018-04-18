8a7c4da2e17d216387ca0ff09380ce5767230d29
NancyWQ <bb08280703@163.com>
2018-04-18 22:29:10 +0800

    2018.4.18晚上

--- miback/app.js
+++ miback/app.js
@@ -1,3 +1,31 @@
 /**
  * Created by wang on 2018/4/16.
  */
+const express=require('express');
+const http=require('http');
+const bodyParser=require('body-parser');
+//注意自己建的模块的引入方式
+const pool=require('./pool.js');
+//创建服务器
+var app=express();
+var server=http.createServer(app);
+server.listen(8081);
+app.get("/carousel",(reg,res)=>{
+    //从前端获取到图片组的标号
+    var pgId=1;
+    var sql="SELECT * FROM  `carousel` where pgid= ? ";
+    pool.getConnection((err,conn)=>{
+        if(err){
+            throw new Error;
+        }else {
+            conn.query(sql,[pgId],(err, result)=> {
+                if (err) {
+                    throw new Error;
+                }else {
+                    res.json(result);
+                }
+                conn.release();
+            })
+        }
+    })
+})
--- miback/pool.js
+++ miback/pool.js
@@ -1,6 +1,7 @@
 /**
  * Created by wang on 2018/4/16.
  */
+    //    安装各个模块 npm install http,express,mysql,body-parser
 const mysql=require('mysql');
 const pool = mysql.createPool({
     host : '127.0.0.1',
@@ -9,4 +10,4 @@ const pool = mysql.createPool({
     user : 'root',
     password : 'root',
 });
-
+module.exports = pool;
